ce5b51b6dbc1d285e6ae8188adaa78605b6dbeec
jeetendrasalgaonkar <jeetendra.salgaonkar@gmail.com>
2022-04-22 08:21:44 +0200

    [AB-0000]: Response object.

---
+++ src/main/kotlin/com/abc/bleeter/bleeterbackend/model/BleetResponse.kt
@@ -0,0 +1,37 @@
+package com.abc.bleeter.bleeterbackend.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.NoArgsConstructor
+import lombok.Setter
+import lombok.extern.jackson.Jacksonized
+import org.springframework.data.annotation.Id
+import java.sql.Timestamp
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+class BleetResponse (
+
+    @Id
+    val bleetId: String,
+
+    /**
+     * The actual Bleet
+     */
+    val bleetMessage: String,
+
+    /**
+     * The person who sent out a bleet.
+     */
+    val bleetUser: String,
+
+    /**
+     * Time at which the bleet was sent out.
+     */
+    val bleetTimestamp: String
+)
