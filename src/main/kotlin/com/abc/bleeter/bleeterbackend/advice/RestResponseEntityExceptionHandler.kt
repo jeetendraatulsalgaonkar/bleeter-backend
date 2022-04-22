f645943b29101ebdf92b36b725af9964acfd927c
jeetendrasalgaonkar <jeetendra.salgaonkar@gmail.com>
2022-04-22 08:12:35 +0200

    [AB-0000]: Added Controller Advice.

---
+++ src/main/kotlin/com/abc/bleeter/bleeterbackend/advice/RestResponseEntityExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.abc.bleeter.bleeterbackend.advice
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(value = [IllegalArgumentException::class, IllegalStateException::class])
+    fun handleConflict(ex: RuntimeException, request: WebRequest): ResponseEntity<Any> {
+        val bodyOfResponse = "This should be application specific"
+        return handleExceptionInternal(
+            ex, bodyOfResponse,
+            HttpHeaders(), HttpStatus.CONFLICT, request
+        )
+    }
+}
